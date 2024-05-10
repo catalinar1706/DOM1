b8b3871de4b8280603fa62c937ee017adbf38b60
Libni Bernate <libnibernate@gmail.com>
2024-05-10 17:09:25 -0500

    Ejercicios exposicion

---
+++ Ejercicios exposicion/grupo1.js
@@ -0,0 +1,10 @@
+// ejercicio del 1 grupo de exposicion 
+
+window.addEventListener('DOMContentLoaded', (Event) => {
+    console.log('DOM completamente cargado y analizado');
+});
+
+function adbrirVentana(){
+    window.open('https//www.ejemplo.com,'_black, 'with = 600, height =400')
+
+};
